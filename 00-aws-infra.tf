23181cf6f3a9ccbd50d67d28f88c30c6c4690d74
Spencer Smith <robertspencersmith@gmail.com>
2017-03-15 10:51:46 -0400

    update egress for aws security group

--- 00-aws-infra.tf
+++ 00-aws-infra.tf
@@ -44,17 +44,13 @@ resource "aws_security_group" "swarm_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
 
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
   }
 
   tags {
